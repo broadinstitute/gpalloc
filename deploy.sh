dbeea9922aeba3aa628eadbf53a58c402f9f856b
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-02-09 16:13:25 -0500

    just do build on circle ci

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
+if [ -z "${SSHCMD}" ]; then
+    echo "FATAL ERROR: SSHCMD undefined."
+    exit 1
+fi
+
 if [ -z "${SSH_USER}" ]; then
     echo "FATAL ERROR: SSH_USER undefined."
-    exit 1
+    exit 2
 fi
 
 if [ -z "${SSH_HOST}" ]; then
     echo "FATAL ERROR: SSH_HOST undefined."
-    exit 2
+    exit 3
 fi
 
 if [ -z "${VAULT_TOKEN}" ]; then
     echo "FATAL ERROR: VAULT_TOKEN undefined."
-    exit 3
+    exit 4
 fi
 
 PROJECT=gpalloc
@@ -27,11 +32,10 @@ COMPOSE_FILE=docker-compose.yml
 
 
 # Start new application container with the current version
-ssh ~/.ssh/broad-dsp-techops $SSH_USER@$SSH_HOST "echo 'on host'"
-#"docker-compose -p $PROJECT -f $COMPOSE_FILE stop"
-#$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE rm -f"
-#$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE pull"
-#$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE up -d"
-#
-## Remove any dangling images that might be hanging around
-#$SSHCMD $SSH_USER@$SSH_HOST "docker images -aq --no-trunc --filter dangling=true | xargs docker rmi || /bin/true"
+$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE stop"
+$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE rm -f"
+$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE pull"
+$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE up -d"
+
+# Remove any dangling images that might be hanging around
+$SSHCMD $SSH_USER@$SSH_HOST "docker images -aq --no-trunc --filter dangling=true | xargs docker rmi || /bin/true"
