a24da582a6dfcacd5c2a7593f162fa1d7f9680fa
Jackie <jacqueline.roberti93@gmail.com>
2018-02-09 17:23:44 -0500

    Jr deploy stuff (#24)

--- deploy.sh
+++ deploy.sh
@@ -15,28 +15,36 @@ if [ -z "${SSH_HOST}" ]; then
     exit 3
 fi
 
-if [ -z "${VAULT_TOKEN}" ]; then
-    echo "FATAL ERROR: VAULT_TOKEN undefined."
+if [ -z "${ENV}" ]; then
+    echo "FATAL ERROR: ENV undefined."
     exit 4
 fi
 
+
 PROJECT=gpalloc
 COMPOSE_FILE=docker-compose.yml
+VAULT_TOKEN=$(cat /etc/vault-token-dsde)
+OUTPUT_DIR=app
+INPUT_DIR=configs
+BRANCH=${BRANCH:-latest}
 
-# Copy over configs & render certs from vault
-# TODO: render configs and copy them onto host
-docker run -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read secret/dsde/dsp-techops/common/server.crt
-docker run -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read secret/dsde/dsp-techops/common/server.key
-docker run -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read secret/dsde/dsp-techops/common/ca-bundle.crt
-# TODO: pull billing acct from vault
+docker run --rm -v $PWD:/working -w /working \
+    -e APP_NAME=$PROJECT \
+    -e VAULT_TOKEN=$VAULT_TOKEN \
+    -e INPUT_DIR=/working/configs \
+    -e OUTPUT_DIR=/working/app \
+    -e IMAGE=$BRANCH \
+    -e ENV=$ENV \
+    broadinstitute/dsde-toolbox:dev configure.rb -y
+
+scp -r $SSHOPTS app/* $SSH_USER@$SSH_HOST:/app
 
 
 # Start new application container with the current version
-$SSHCMD $SSH_USER@$SSH_HOST "echo 'on host'"
-#"docker-compose -p $PROJECT -f $COMPOSE_FILE stop"
-#$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE rm -f"
-#$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE pull"
-#$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE up -d"
-#
-## Remove any dangling images that might be hanging around
-#$SSHCMD $SSH_USER@$SSH_HOST "docker images -aq --no-trunc --filter dangling=true | xargs docker rmi || /bin/true"
+$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE stop"
+$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE rm -f"
+$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE pull"
+$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE up -d"
+
+# Remove any dangling images that might be hanging around
+$SSHCMD $SSH_USER@$SSH_HOST "docker images -aq --no-trunc --filter dangling=true | xargs docker rmi || /bin/true"
