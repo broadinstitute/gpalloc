760dd0662c7eea847fe7bfcc1aca39ea3e4068a6
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-02-12 16:44:37 -0500

    changes to deploy script :)

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ if [ -z "${SSH_USER}" ]; then
     exit 2
 fi
 
-if [ -z "${SSH_HOST}" ]; then
-    echo "FATAL ERROR: SSH_HOST undefined."
+if [ -z "${HOST}" ]; then
+    echo "FATAL ERROR: HOST undefined."
     exit 3
 fi
 
@@ -26,25 +26,25 @@ COMPOSE_FILE=docker-compose.yml
 VAULT_TOKEN=$(cat /etc/vault-token-dsde)
 OUTPUT_DIR=app
 INPUT_DIR=configs
-BRANCH=${BRANCH:-develop}
+BRANCH=${IMAGE:-develop}
 
 docker run --rm -v $PWD:/working -w /working \
     -e APP_NAME=$PROJECT \
     -e VAULT_TOKEN=$VAULT_TOKEN \
     -e INPUT_DIR=/working/configs \
     -e OUTPUT_DIR=/working/app \
-    -e IMAGE=$BRANCH \
+    -e IMAGE=$IMAGE \
     -e ENV=$ENV \
     broadinstitute/dsde-toolbox:dev ruby configure.rb -y
 
-scp -r $SSHOPTS app/* $SSH_USER@$SSH_HOST:/app
+scp -r $SSHOPTS app/* $SSH_USER@$HOST:/app
 
 
 # Start new application container with the current version
-$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE stop"
-$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE rm -f"
-$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE pull"
-$SSHCMD $SSH_USER@$SSH_HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE up -d"
+$SSHCMD $SSH_USER@$HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE stop"
+$SSHCMD $SSH_USER@$HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE rm -f"
+$SSHCMD $SSH_USER@$HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE pull"
+$SSHCMD $SSH_USER@$HOST "docker-compose -p $PROJECT -f $COMPOSE_FILE up -d"
 
 # Remove any dangling images that might be hanging around
-$SSHCMD $SSH_USER@$SSH_HOST "docker images -aq --no-trunc --filter dangling=true | xargs docker rmi || /bin/true"
+$SSHCMD $SSH_USER@$HOST "docker images -aq --no-trunc --filter dangling=true | xargs docker rmi || /bin/true"
