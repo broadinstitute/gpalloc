0f4febbe27f4df2eeb9d447e55a67cffd00e2dd0
Hussein Elgridly <hussein@broadinstitute.org>
2018-02-13 15:25:04 -0500

    words and endings

--- scripts/nuke_projects.sh
+++ scripts/nuke_projects.sh
@@ -5,4 +5,9 @@
 
 # You should only run this in conjunction with dropping all tables on the corresponding gpalloc.
 
-gcloud projects list | grep gpalloc | cut -f 1 -d ' ' | xargs -I % gcloud projects delete % --quiet
+if [[ $# == 0 ]]; then
+	echo "Provide a prefix. Typically you want some variant of gpalloc-[env]-[branch], e.g. nuke_projects.sh gpalloc-test-develop"
+	exit 0
+fi
+
+gcloud projects list | grep $1 | cut -f 1 -d ' ' | xargs -I % gcloud projects delete % --quiet
