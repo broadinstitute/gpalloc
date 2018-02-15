d88d3ea0be1542f9d104390d6b6555b719b487cd
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-02-15 15:39:04 -0500

    just put it in a script because blergh

---
+++ trigger-jenkins-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+wget -q --auth-no-challenge --user $JENKINS_USER --password $JENKINS_API_TOKEN --output-document crumb 'https://fc-jenkins.dsp-techops.broadinstitute.org/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
+curl -X --POST https://fc-jenkins.dsp-techops.broadinstitute.org/job/gpalloc-deploy/buildWithParameters?token=$GPALLOC_DEPLOY_TOKEN --user $JENKINS_USER:$JENKINS_API_TOKEN --data-urlencode json='{"parameter": [{"name":"IMAGE", "value":"$CIRCLE_BRANCH"}]}' -H '$(cat crumb)'
