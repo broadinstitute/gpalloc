7722ce954ef74480720d1ba994f179ce025cbee0
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-02-20 10:06:08 -0500

    debug deploy

--- trigger-jenkins-deploy.sh
+++ trigger-jenkins-deploy.sh
@@ -2,4 +2,6 @@
 set -e
 
 wget -q --auth-no-challenge --user $JENKINS_USER --password $JENKINS_API_TOKEN --output-document crumb 'https://fc-jenkins.dsp-techops.broadinstitute.org/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
+cat crumb
+echo "running curl now"
 curl -X --POST https://fc-jenkins.dsp-techops.broadinstitute.org/job/gpalloc-deploy/buildWithParameters?token=$GPALLOC_DEPLOY_TOKEN --user $JENKINS_USER:$JENKINS_API_TOKEN --data-urlencode json='{"parameter": [{"name":"IMAGE", "value":"$CIRCLE_BRANCH"}]}' -H '$(cat crumb)'
