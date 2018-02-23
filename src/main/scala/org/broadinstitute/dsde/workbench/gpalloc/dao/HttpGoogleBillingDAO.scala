bc6f55c7b777367efdc095339fb309882a39696c
Hussein Elgridly <hussein@broadinstitute.org>
2018-02-23 17:23:36 -0500

    back to 429 handling

--- src/main/scala/org/broadinstitute/dsde/workbench/gpalloc/dao/HttpGoogleBillingDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/gpalloc/dao/HttpGoogleBillingDAO.scala
@@ -45,7 +45,7 @@ class HttpGoogleBillingDAO(appName: String,
                            billingEmail: String,
                            opsThrottle: Int,
                            opsThrottlePerDuration: FiniteDuration)
-                           (implicit val system: ActorSystem, val executionContext: ExecutionContext)
+                          (implicit val system: ActorSystem, val executionContext: ExecutionContext)
   extends GoogleDAO with GoogleUtilities {
 
   protected val workbenchMetricBaseName = "billing"
@@ -67,7 +67,7 @@ class HttpGoogleBillingDAO(appName: String,
   val saScopes = Seq(
     "https://www.googleapis.com/auth/cloud-billing",
     ComputeScopes.CLOUD_PLATFORM
-   )
+  )
 
   val credential: Credential = {
     new GoogleCredential.Builder()
@@ -141,16 +141,26 @@ class HttpGoogleBillingDAO(appName: String,
     // there is not much else to be done... too bad scala does not have duck typing.
     operation.operationType match {
       case CreatingProject =>
-        opThrottler.throttle(() => retryWhen500orGoogleError(() => {
+        opThrottler.throttle(() => retryWithRecoverWhen500orGoogleError(() => {
           executeGoogleRequest(cloudResources.operations().get(operation.operationId))
-        })).map { op =>
+        }) {
+          case t: HttpResponseException if t.getStatusCode == 429 =>
+            //429 is Too Many Requests. If we get this back, just say we're not done yet and try again later
+            logger.warn(s"Google 429 for pollOperation ${operation.billingProjectName} ${operation.operationType}. Retrying next round...")
+            new CRMOperation().setDone(false).setError(null)
+        }).map { op =>
           operation.copy(done = toScalaBool(op.getDone), errorMessage = Option(op.getError).map(error => toErrorMessage(error.getMessage, error.getCode)))
         }
 
       case EnablingServices =>
-        opThrottler.throttle(() => retryWhen500orGoogleError(() => {
+        opThrottler.throttle(() => retryWithRecoverWhen500orGoogleError(() => {
           executeGoogleRequest(servicesManager.operations().get(operation.operationId))
-        })).map { op =>
+        }) {
+          case t: HttpResponseException if t.getStatusCode == 429 =>
+            //429 is Too Many Requests. If we get this back, just say we're not done yet and try again later
+            logger.warn(s"Google 429 for pollOperation ${operation.billingProjectName} ${operation.operationType}. Retrying next round...")
+            new SMOperation().setDone(false).setError(null)
+        }).map { op =>
           operation.copy(done = toScalaBool(op.getDone), errorMessage = Option(op.getError).map(error => toErrorMessage(error.getMessage, error.getCode)))
         }
     }
@@ -169,7 +179,7 @@ class HttpGoogleBillingDAO(appName: String,
     }).recover {
       case t: HttpResponseException if StatusCode.int2StatusCode(t.getStatusCode) == StatusCodes.Conflict =>
         throw GoogleProjectConflict(projectName)
-    } map ( googleOperation => {
+    } map (googleOperation => {
       if (toScalaBool(googleOperation.getDone) && Option(googleOperation.getError).exists(_.getCode == Code.ALREADY_EXISTS.value())) {
         throw GoogleProjectConflict(projectName)
       }
@@ -205,8 +215,8 @@ class HttpGoogleBillingDAO(appName: String,
 
     // all of these things should be idempotent
     for {
-    // set the billing account
-      billing <- opThrottler.throttle( () => retryWhen500orGoogleError(() => {
+      // set the billing account
+      billing <- opThrottler.throttle(() => retryWhen500orGoogleError(() => {
         executeGoogleRequest(billingManager.projects().updateBillingInfo(projectResourceName, new ProjectBillingInfo().setBillingEnabled(true).setBillingAccountName(billingAccount)))
       }))
 
@@ -224,7 +234,7 @@ class HttpGoogleBillingDAO(appName: String,
 
     // all of these things should be idempotent
     for {
-    // create project usage export bucket
+      // create project usage export bucket
       exportBucket <- retryWithRecoverWhen500orGoogleError(() => {
         val bucket = new Bucket().setName(usageBucketName)
         executeGoogleRequest(storage.buckets.insert(projectName, bucket))
@@ -232,7 +242,9 @@ class HttpGoogleBillingDAO(appName: String,
 
       // create bucket for workspace bucket storage/usage logs
       storageLogsBucket <- createStorageLogsBucket(projectName)
-      _ <- retryWhen500orGoogleError(() => { allowGoogleCloudStorageWrite(storageLogsBucket) })
+      _ <- retryWhen500orGoogleError(() => {
+        allowGoogleCloudStorageWrite(storageLogsBucket)
+      })
 
       googleProject <- getGoogleProject(projectName)
 
@@ -283,11 +295,11 @@ class HttpGoogleBillingDAO(appName: String,
 
   def cleanupPolicyBindings(projectName: String): Future[Unit] = {
     for {
-      existingPolicy <- opThrottler.throttle( () => retryWhen500orGoogleError(() => {
+      existingPolicy <- opThrottler.throttle(() => retryWhen500orGoogleError(() => {
         executeGoogleRequest(cloudResources.projects().getIamPolicy(projectName, null))
       }))
 
-      _ <- opThrottler.throttle( () => retryWhen500orGoogleError(() => {
+      _ <- opThrottler.throttle(() => retryWhen500orGoogleError(() => {
         val existingPolicies: Map[String, Seq[String]] = existingPolicy.getBindings.asScala.map { policy => policy.getRole -> policy.getMembers.asScala }.toMap
 
         val updatedPolicies = existingPolicies.map { case (role, members) =>
@@ -305,7 +317,7 @@ class HttpGoogleBillingDAO(appName: String,
   }
 
   //stolen: https://gist.github.com/ryanlecompte/6313683
-  def sequentially[A,T](items: Seq[A])(f: A => Future[T]): Future[Unit] = {
+  def sequentially[A, T](items: Seq[A])(f: A => Future[T]): Future[Unit] = {
     items.headOption match {
       case Some(nextItem) =>
         val fut = f(nextItem)
@@ -323,11 +335,11 @@ class HttpGoogleBillingDAO(appName: String,
   def cleanupCromwellAuthBucket(billingProjectName: String): Future[Unit] = {
     val bucketName = cromwellAuthBucketName(billingProjectName)
     for {
-      oAcls <- googleRq( storage.defaultObjectAccessControls.list(bucketName) )
+      oAcls <- googleRq(storage.defaultObjectAccessControls.list(bucketName))
       deleteOAcls = oAcls.getItems.asScala.filter(a => shouldDelete(a.getEntity))
       _ <- sequentially(deleteOAcls) { d => googleRq(storage.defaultObjectAccessControls.delete(bucketName, d.getEntity)) }
 
-      bAcls <- googleRq( storage.bucketAccessControls.list(bucketName) )
+      bAcls <- googleRq(storage.bucketAccessControls.list(bucketName))
       deleteBAcls = bAcls.getItems.asScala.filter(a => shouldDelete(a.getEntity))
       _ <- sequentially(deleteBAcls) { d => googleRq(storage.bucketAccessControls.delete(bucketName, d.getEntity)) }
     } yield {
@@ -337,12 +349,14 @@ class HttpGoogleBillingDAO(appName: String,
 
   //dear god, google. surely there's a better way
   def gProjectPath(project: String) = s"projects/$project"
+
   def gSAPath(project: String, serviceAccountEmail: String) = gProjectPath(project) + s"/serviceAccounts/$serviceAccountEmail"
+
   def gKeyPath(project: String, serviceAccountEmail: String, keyEmail: String) = gSAPath(project, serviceAccountEmail) + s"/keys/$keyEmail"
 
   def cleanupPetSAKeys(projectName: String): Future[Unit] = {
     for {
-      serviceAccounts <- googleRq( iam.projects().serviceAccounts().list(gProjectPath(projectName)) )
+      serviceAccounts <- googleRq(iam.projects().serviceAccounts().list(gProjectPath(projectName)))
       pets = serviceAccounts.getAccounts.asScala.filter(_.getEmail.startsWith("pet-"))
       _ <- removeKeysForPets(projectName, pets)
     } yield {
@@ -351,7 +365,7 @@ class HttpGoogleBillingDAO(appName: String,
   }
 
   def removeKeysForPets(projectName: String, pets: Seq[ServiceAccount]): Future[Unit] = {
-    sequentially(pets){ pet =>
+    sequentially(pets) { pet =>
       for {
         petKeys <- googleRq(iam.projects.serviceAccounts.keys.list(gSAPath(projectName, pet.getEmail)))
         _ <- removeKeysForPet(projectName, pet.getEmail, petKeys.getKeys.asScala)
@@ -362,7 +376,7 @@ class HttpGoogleBillingDAO(appName: String,
   }
 
   def removeKeysForPet(projectName: String, petEmail: String, petKeys: Seq[ServiceAccountKey]): Future[Unit] = {
-    sequentially(petKeys){ petKey =>
+    sequentially(petKeys) { petKey =>
       for {
         _ <- googleRq(iam.projects.serviceAccounts.keys.delete(gKeyPath(projectName, petEmail, petKey.getName)))
       } yield {
