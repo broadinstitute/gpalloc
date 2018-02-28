0a635df60d4f5d70345a3d56865df03cf364df17
Hussein Elgridly <hussein@broadinstitute.org>
2018-02-28 15:15:31 -0500

    release script

---
+++ scripts/release_master.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "WARNING: You are about to force-push your local develop branch onto broadinstitute/gpalloc:master."
+echo "If you really want to do this, type the word \"release\" and hit [ENTER]:"
+
+read release
+
+if [ $release = "release" ];
+then
+	git push origin develop:master --force
+else
+	echo "escape!"
+fi
+
